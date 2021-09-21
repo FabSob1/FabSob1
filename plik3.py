b39684249561053863e8afbbd721a15a2d277948
Pracownia <example@gmail.com>
2021-09-21 19:42:06 +0200

    second commit

--- plik3.py
+++ plik3.py
@@ -1,9 +1,10 @@
 import math
+import sys
 
-a = int(input())
-b = int(input())
-c = int(input())
-
+print ("Podaj wartosci funkcji: a^2x + bx +c: ")
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
 #a^2x + bx + c
 def fun(a, b, c):
     b = b*b
@@ -11,7 +12,12 @@ def fun(a, b, c):
     if delta >0:
         x1 = (-b  -  math.sqrt(delta))/2*a
         x2 = (-b + math.sqrt(delta))/2*a
+    elif delta == 0:
+        x1 = -b/2*a
         print (x1)
         print (x2)
+
+    else : 
+        print ("brak rozwiazan")
 
 fun(a,b,c)
