4b2c5b4787454519c8bed8c383e720d720d05593
Pracownia <example@gmail.com>
2021-09-21 19:47:10 +0200

    adding print in if

--- plik3.py
+++ plik3.py
@@ -12,10 +12,12 @@ def fun(a, b, c):
     if delta >0:
         x1 = (-b  -  math.sqrt(delta))/2*a
         x2 = (-b + math.sqrt(delta))/2*a
+        print (x1)
+        print (x2)
+
     elif delta == 0:
         x1 = -b/2*a
         print (x1)
-        print (x2)
 
     else : 
         print ("brak rozwiazan")
